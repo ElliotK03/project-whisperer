7a678b4848358a576108c4c1b5f83cb7b6de2d9e
ElliotK03 <164868526+ElliotK03@users.noreply.github.com>
2025-01-18 20:56:20 +0800

    Refactor bot.js to load environment variables correctly and simplify token assignment

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 //guilded bot 
+require('dotenv').config()
 const { Client } = require("guilded.js");
-
-const token = process.env.GUILDED_TOKEN || require('dotenv').config();
+const token = process.env.GUILDED_TOKEN;
 const client = new Client({ token });
 
 client.on("ready", () => {
