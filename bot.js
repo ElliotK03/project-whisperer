63ba6c0fb265caf3071eb4e1cfcc70770ec8a42a
ElliotK03 <164868526+ElliotK03@users.noreply.github.com>
2025-01-18 19:34:10 +0800

    Updated Guilded bot to take token from env

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 //guilded bot 
 const { Client } = require("guilded.js");
 
-const token = require("./token.json").token;
+const token = (require("./token.json").token) || process.env.GUILDED_TOKEN;
 const client = new Client({ token });
 
 client.on("ready", () => {
